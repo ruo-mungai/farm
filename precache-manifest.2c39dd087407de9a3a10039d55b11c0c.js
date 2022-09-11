1a88d7cc6e34891f6c0ae128e0b8f8c167803170
Patrick <ruomungai@gmail.com>
2022-09-11 21:14:47 +0300

    Updates

---
+++ precache-manifest.2c39dd087407de9a3a10039d55b11c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ba502558a912716358126e3282707f",
+    "url": "/farm/index.html"
+  },
+  {
+    "revision": "e4efd1b0885d88df23cf",
+    "url": "/farm/static/css/main.4a7ca07d.chunk.css"
+  },
+  {
+    "revision": "d4f4a3618a551d8bf271",
+    "url": "/farm/static/js/2.b9cb9192.chunk.js"
+  },
+  {
+    "revision": "dba0c006430f86ab5dfd81528eb42c84",
+    "url": "/farm/static/js/2.b9cb9192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4efd1b0885d88df23cf",
+    "url": "/farm/static/js/main.a191ad98.chunk.js"
+  },
+  {
+    "revision": "234609486ef03a253e73",
+    "url": "/farm/static/js/runtime-main.a68186d6.js"
+  }
+]);
